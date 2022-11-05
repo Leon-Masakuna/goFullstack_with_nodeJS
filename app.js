efd2b617effe192d1e1a1f6343d58a2ae679061e
Leon-Masakuna <masakunamfengleon@gmail.com>
2022-11-05 19:47:01 +0100

    fix : connection to mongoDB

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const app = express();
 const mongoose = require("mongoose");
+const Thing = require("./models/thing");
 
 mongoose
   .connect(
@@ -27,13 +28,36 @@ app.use((req, res, next) => {
 });
 
 app.post("/api/stuff", (req, res, next) => {
+  delete req.body._id;
+  const thing = new Thing({
+    ...req.body,
+  });
+  thing
+    .save()
+    .then(() => res.status(201).json({ message: "Objet enregistré !" }))
+    .catch((error) => res.status(400).json({ error }));
+});
+
+app.get("/api/stuff/:id", (req, res, next) => {
+  Thing.findOne({ _id: req.params.id })
+    .then((thing) => res.status(200).json(thing))
+    .catch((error) => res.status(404).json({ error }));
+});
+
+app.get("/api/stuff", (req, res, next) => {
+  Thing.find()
+    .then((things) => res.status(200).json(things))
+    .catch((error) => res.status(400).json({ error }));
+});
+
+/* app.post("/api/stuff", (req, res, next) => {
   console.log(req.body);
   res.status(201).json({
     message: "Objet créé avec succes",
   });
-});
+}); */
 
-app.get("/api/stuff", (req, res, next) => {
+/* app.get("/api/stuff", (req, res, next) => {
   const stuff = [
     {
       _id: "oeihfzeoi",
@@ -55,7 +79,7 @@ app.get("/api/stuff", (req, res, next) => {
     },
   ];
   res.status(200).json(stuff);
-});
+}); */
 
 /* app.use((req, res, next) => {
   console.log("Requêtes reçues");

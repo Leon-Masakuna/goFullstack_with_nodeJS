bda2cb90dbc18a4e06b64df3b3a10793b2044c14
Leon-Masakuna <masakunamfengleon@gmail.com>
2022-11-07 15:32:49 +0100

    feat : allow all origins to request our api

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Thing = require("./models/thing");
+const stuffRoutes = require("./routes/stuff");
 
 mongoose
   .connect(
@@ -27,7 +28,9 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post("/api/stuff", (req, res, next) => {
+app.use("/api/stuff", stuffRoutes);
+
+/* app.post("/api/stuff", (req, res, next) => {
   delete req.body._id;
   const thing = new Thing({
     ...req.body,
@@ -35,6 +38,18 @@ app.post("/api/stuff", (req, res, next) => {
   thing
     .save()
     .then(() => res.status(201).json({ message: "Objet enregistré !" }))
+    .catch((error) => res.status(400).json({ error }));
+});
+
+app.put("/api/stuff/:id", (req, res, next) => {
+  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: "Objet modifié !" }))
+    .catch((error) => res.status(400).json({ error }));
+});
+
+app.delete("/api/stuff/:id", (req, res, next) => {
+  Thing.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: "Objet supprimé !" }))
     .catch((error) => res.status(400).json({ error }));
 });
 
@@ -48,7 +63,7 @@ app.get("/api/stuff", (req, res, next) => {
   Thing.find()
     .then((things) => res.status(200).json(things))
     .catch((error) => res.status(400).json({ error }));
-});
+}); */
 
 /* app.post("/api/stuff", (req, res, next) => {
   console.log(req.body);
